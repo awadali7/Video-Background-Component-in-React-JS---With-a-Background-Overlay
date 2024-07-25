229c1e16b8d122655f3e2ade8968ec580b67a1cb
awadali <mailtoawadali@gmail.com>
2024-07-25 12:24:45 +0530

    final update

---
+++ src/components/spotlight-vedio.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import videoBg from "../assets/car-service-spotlight.mp4";
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="overlay"></div>
+            <video src={videoBg} autoPlay loop muted />
+            <div className="content">
+                <h1>Welcome</h1>
+                <p>To my site.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
